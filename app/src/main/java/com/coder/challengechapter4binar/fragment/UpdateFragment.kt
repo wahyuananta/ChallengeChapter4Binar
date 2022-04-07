dc91e526c0255aa2e3fa01ac1ed2c1bf6ebd501d
wahyuananta <wahyuanantajr@gmail.com>
2022-04-07 13:42:52 +0700

    Implement update in update fragment

--- app/src/main/java/com/coder/challengechapter4binar/fragment/UpdateFragment.kt
+++ app/src/main/java/com/coder/challengechapter4binar/fragment/UpdateFragment.kt
@@ -1,20 +1,73 @@
 package com.coder.challengechapter4binar.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.coder.challengechapter4binar.R
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.coder.challengechapter4binar.AppDatabase
+import com.coder.challengechapter4binar.Club
+import com.coder.challengechapter4binar.databinding.FragmentUpdateBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 
-class UpdateFragment : Fragment() {
+class UpdateFragment(var itemSelected : Club) : DialogFragment() {
+    private var _binding: FragmentUpdateBinding? = null
+    private val binding get() = _binding!!
+    var mDb: AppDatabase?=null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        _binding = FragmentUpdateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mDb = AppDatabase.getInstance(requireContext())
+
+        binding.btnUpdate.setOnClickListener {
+            val tanggal = binding.etTanggal.text
+            val jam = binding.etJam.text
+
+            when {
+                tanggal.isNullOrEmpty() -> {
+                    binding.ilTanggal.error = "Tanggal belum diisi"
+                }
+                jam.isNullOrEmpty() -> {
+                    binding.ilJam.error = "Kick off belum diisi"
+                }
+                else -> {
+                    val dataClub = itemSelected
+                    dataClub.tanggal = tanggal.toString()
+                    dataClub.jam = jam.toString()
+
+                    GlobalScope.async {
+                        val result = mDb?.clubDao()?.updateClub(dataClub)
+                        activity?.runOnUiThread {
+                            if (result != 0) {
+                                Toast.makeText(it.context, "Jadwal pertandingan berhasil diupdate", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(it.context, "Gagal mengupdate jadwal pertandingan", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                    dialog?.dismiss()
+                }
+            }
+        }
+        binding.btnBatal.setOnClickListener {
+            dialog?.dismiss()
+        }
     }
 
 }
