be1aa841124e284eec99d41d7b33e07072d4ee6b
wahyuananta <wahyuanantajr@gmail.com>
2022-04-07 12:34:39 +0700

    Add main navigation

--- app/src/main/java/com/coder/challengechapter4binar/fragment/AddFragment.kt
+++ app/src/main/java/com/coder/challengechapter4binar/fragment/AddFragment.kt
@@ -5,9 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.coder.challengechapter4binar.R
 
-class AddFragment : Fragment() {
+class AddFragment : DialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
