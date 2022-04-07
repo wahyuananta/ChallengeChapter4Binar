801013c6403e1a304d7381028bd8b6683da8f3f3
wahyuananta <wahyuanantajr@gmail.com>
2022-04-07 12:41:17 +0700

    Add club in app database

--- app/src/main/java/com/coder/challengechapter4binar/AppDatabase.kt
+++ app/src/main/java/com/coder/challengechapter4binar/AppDatabase.kt
@@ -5,9 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [DataUser::class], version = 1)
+@Database(entities = [DataUser::class, Club::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun dataUserDao(): DataUserDao
+    abstract fun clubDao(): ClubDao
 
     companion object {
         private var INSTANCE : AppDatabase? = null
