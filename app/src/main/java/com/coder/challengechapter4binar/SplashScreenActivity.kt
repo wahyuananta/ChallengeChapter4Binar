b535a561107b496bca861d8fa46d0e0c40f05bb9
wahyuananta <wahyuanantajr@gmail.com>
2022-04-07 16:23:55 +0700

    Add lottie in splashScreen

--- app/src/main/java/com/coder/challengechapter4binar/SplashScreenActivity.kt
+++ app/src/main/java/com/coder/challengechapter4binar/SplashScreenActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.coder.challengechapter4binar.databinding.ActivitySplashScreenBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
@@ -13,12 +17,17 @@ class SplashScreenActivity : AppCompatActivity() {
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.ivBola.alpha = 0f
-        binding.ivBola.animate().setDuration(1500).alpha(1f).withEndAction {
-            val intentKeMain = Intent(this, MainActivity::class.java)
-            startActivity(intentKeMain)
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-            finish()
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(4000L)
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
         }
+
+//        binding.ivBola.alpha = 0f
+//        binding.ivBola.animate().setDuration(1500).alpha(1f).withEndAction {
+//            val intentKeMain = Intent(this, MainActivity::class.java)
+//            startActivity(intentKeMain)
+//            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+//            finish()
+//        }
     }
 }
